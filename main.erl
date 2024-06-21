a1bd6252347212d924c846476bbfcb9d5c72594a
JojoBR07 <joilsontelles@gmail.com>
2024-06-20 21:12:00 -0300

    Feat: changes

--- main.erl
+++ main.erl
@@ -8,15 +8,14 @@ start() ->
     %% Inicialização do buffer
     Buffer = spawn(buffer, start, []),
 
-    %% Inicialização do produtor
+    %% Inicialização dos produtores
     P1 = spawn(produtor, start, [Buffer]),
     P2 = spawn(produtor, start, [Buffer]),
 
-    %% Inicialização do consumidor
+    %% Inicialização dos consumidores
     C1 = spawn(consumidor, start, [Buffer]),
     C2 = spawn(consumidor, start, [Buffer]),
     C3 = spawn(consumidor, start, [Buffer]),
     C4 = spawn(consumidor, start, [Buffer]),
-    C5 = spawn(consumidor, start, [Buffer]),
 
-    {Buffer, P1, P2, C1, C2, C3, C4, C5}.
+    {Buffer, P1, P2, C1, C2, C3, C4}.
