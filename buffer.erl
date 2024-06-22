a5441e1aaa800b50a5a2577f605c344416af2775
JojoBR07 <joilsontelles@gmail.com>
2024-06-22 00:31:01 -0300

    Feat: change buffer

--- buffer.erl
+++ buffer.erl
@@ -4,3 +4,25 @@
 start() ->
     %% Mensagem de console para inicialização do buffer
     io:format("[Buffer] - Buffer Iniciado").
+
+buffer(Products) ->
+
+    %% Espera por mensagens de produção e consumo
+    receive
+
+        %% Adiciona itens ao buffer quando recebe mensagens do produtor
+        {produtor, Item} ->
+            NewProducts = [Item | Products],
+            buffer(NewProducts);
+
+        %% Retorna itens (ou vazio) para consumidores quando recebe solicitação
+        {consumidor, PidConsumidor} ->
+            case Products of
+                [] ->
+                    PidConsumidor ! {empty},
+                    buffer(Products);
+                [Product | AllProducts] ->
+                    PidConsumidor ! {consumidor, Product},
+                    buffer(AllProducts)
+            end
+    end.
