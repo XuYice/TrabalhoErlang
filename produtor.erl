9113b922cfc9ded4d263942c804e903b489c6aca
JojoBR07 <joilsontelles@gmail.com>
2024-06-22 17:09:08 -0300

    Feat: change buffer

--- produtor.erl
+++ produtor.erl
@@ -1,14 +1,14 @@
 -module(produtor).
--export([start/4, produce/1]).
+-export([start/4).
 
 start(IdP, IdB, T, Buffer) ->
     %% Mensagem de console para inicialização do produtor
     io:format("[Produtor] - Produtor Iniciado ~n"),
 
     %% Chamada do produtor
-    produce(Buffer).    
+    loop(Buffer).    
 
-produce(Buffer) ->
+loop(Buffer) ->
     %% This is a simple producer that generates an item and sends it to the buffer
     Buffer ! {produce, some_item},
-    produce(Buffer).
+    loop(Buffer).
